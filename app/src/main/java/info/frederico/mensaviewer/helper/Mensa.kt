0d00915825771cacce9aec519eac19511258f36d
Frederico Bormann <freddy.bormann@hotmail.de>
2018-07-16 08:31:49 +0200

    fixed showing today's plan

--- app/src/main/java/info/frederico/mensaviewer/helper/Mensa.kt
+++ app/src/main/java/info/frederico/mensaviewer/helper/Mensa.kt
@@ -4,7 +4,7 @@ package info.frederico.mensaviewer.helper
  * Created by fredd on 14.07.2018.
  */
 enum class Mensa(val url : String) {
-    STUDIERENDENHAUS("https://speiseplan.studierendenwerk-hamburg.de/de/310/2018/99/"),
-    INFORMATIKUM("https://speiseplan.studierendenwerk-hamburg.de/de/580/2018/99/"),
-    CAMPUS("https://speiseplan.studierendenwerk-hamburg.de/de/340/2018/99/")
+    STUDIERENDENHAUS("https://speiseplan.studierendenwerk-hamburg.de/de/310/2018/0/"),
+    INFORMATIKUM("https://speiseplan.studierendenwerk-hamburg.de/de/580/2018/0/"),
+    CAMPUS("https://speiseplan.studierendenwerk-hamburg.de/de/340/2018/0/")
 }
