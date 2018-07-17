34f68464e03857a60a279d685565a451d23918ca
Timon Engelke <timon.engelke@online.de>
2018-07-17 13:49:20 +0200

    Use a hashmap for allergens by ingredient

--- app/src/main/java/info/frederico/mensaviewer/helper/Essen.kt
+++ app/src/main/java/info/frederico/mensaviewer/helper/Essen.kt
@@ -2,7 +2,7 @@ package info.frederico.mensaviewer.helper
 
 data class Essen(
         val bezeichnung: String,
-        val allergene: List<String>,
+        val allergene: HashMap<String, List<String>>,
         val studentenPreis: String,
         val bedienstetePreis: String,
         val gaestePreis: String)
