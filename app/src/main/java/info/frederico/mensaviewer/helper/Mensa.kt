3a161175d8e0eb78d4db7a5e86f657c34c809cfc
Frederico Bormann <freddy.bormann@hotmail.de>
2018-08-04 15:40:55 +0200

    Change enum Mensa to include description and icon

--- app/src/main/java/info/frederico/mensaviewer/helper/Mensa.kt
+++ app/src/main/java/info/frederico/mensaviewer/helper/Mensa.kt
@@ -1,10 +1,14 @@
 package info.frederico.mensaviewer.helper
 
+import android.support.v7.content.res.AppCompatResources.getDrawable
+import info.frederico.mensaviewer.R
+
 /**
  * Created by fredd on 14.07.2018.
  */
-enum class Mensa(val url : String) {
-    STUDIERENDENHAUS("https://speiseplan.studierendenwerk-hamburg.de/de/310/2018/0/"),
-    INFORMATIKUM("https://speiseplan.studierendenwerk-hamburg.de/de/580/2018/0/"),
-    CAMPUS("https://speiseplan.studierendenwerk-hamburg.de/de/340/2018/0/")
+enum class Mensa(val description : String, val icon : Int, val url : String) {
+    STUDIERENDENHAUS("Studierendenhaus", R.drawable.ic_home_black_24dp, "https://speiseplan.studierendenwerk-hamburg.de/de/310/2018/0/"),
+    INFORMATIKUM("Informatikum", R.drawable.ic_baseline_computer_24px, "https://speiseplan.studierendenwerk-hamburg.de/de/580/2018/0/"),
+    CAMPUS("Campus", R.drawable.ic_baseline_account_balance_24px, "https://speiseplan.studierendenwerk-hamburg.de/de/340/2018/0/"),
+    HARBURG("Harburg", R.drawable.ic_dashboard_black_24dp, "https://speiseplan.studierendenwerk-hamburg.de/de/570/2018/0/")
 }
